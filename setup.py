599f3962de62630ca81d8cdbd161afc15bdcc700
flma0001 <markus.fleschutz@hs-karlsruhe.de>
2021-07-22 15:09:18 +0200

    Change description and MANIFEST

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version=__version__,
     author="Markus Fleschutz",
     author_email="mfleschutz@gmail.com",
-    description="Electricity market data",
+    description="Dynamic electricity carbon emission factors and prices for Europe",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DrafProject/elmada",
